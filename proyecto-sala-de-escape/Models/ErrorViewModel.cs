1ad2a2b635e6f211580c6655bbcb31a87574d12b
AlonKanter <67279070+AlonKanter@users.noreply.github.com>
2024-07-08 19:59:42 -0300

    Creacion MVC

---
+++ proyecto-sala-de-escape/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyecto_sala_de_escape.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
