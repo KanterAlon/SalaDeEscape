916b903e99f389fb743bcb0b9cf55c2e6acc2613
Felipe Otero <48389979@est.ort.edu.ar>
2024-07-10 10:24:47 -0300

    creacion clase escape.cs

---
+++ Models/Escape.cs
@@ -0,0 +1,52 @@
+public static class Escape
+{
+   
+    private static string[] incognitasSalas;
+    private static int estadoJuego;
+
+ 
+    private static void InicializarJuego()
+    {
+     
+        incognitasSalas = new string[]
+        {
+            "",
+            "",
+           
+        };
+
+        estadoJuego = 1; 
+    }
+
+
+    public static int GetEstadoJuego()
+    {
+        return estadoJuego;
+    }
+
+
+    public static void AvanzarSala()
+    {
+        if (estadoJuego < incognitasSalas.Length)
+        {
+            estadoJuego++;
+        }
+     
+    }
+
+   
+    public static bool ValidarRespuesta(string respuesta)
+    {
+
+        string solucion = incognitasSalas[estadoJuego - 1]; 
+
+     
+        return respuesta == solucion;
+    }
+
+   
+    public static void ReiniciarJuego()
+    {
+        InicializarJuego(); 
+    }
+}
