383c67de116eb2a6ffe39ec19754187272179036
AlonKanter <67279070+AlonKanter@users.noreply.github.com>
2024-07-30 21:47:30 -0300

    Terminacion Terminal

--- obj/Debug/net8.0/proyecto-sala-de-escape.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyecto-sala-de-escape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto-sala-de-escape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7a7f31c7bae5b7862eb9d70785096ed53d7568c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca3144a3918b65898836df1852d152a72283dab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto-sala-de-escape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto-sala-de-escape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
